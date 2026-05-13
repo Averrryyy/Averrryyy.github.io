94d79c9c3bedb10e5070075479ebc3591e3d9b07
Avery Robertson <avery.br125@gmail.com>
2026-05-13 11:53:47 -0400

    fixerd a bug involving white spaces

--- questionReader.java
+++ questionReader.java
@@ -9,8 +9,13 @@ class Main {
             try{
                 Stream<String> s = Files.lines(Paths.get(filename));
                 Object[] items = s.toArray();
+                int blankCountOffset = 0;
                 for(int i = 0; i < items.length; i ++){
-                    System.out.println("<li> <input type=\"checkbox\" id=\""+ (i+1) + "\"> <label for=\""+ (i+1) +"\">" + items[i]  + "</label> </li>");
+                    if(items[i].toString().isEmpty()){
+                        blankCountOffset++;
+                    }else{
+                        System.out.println("<li> <input type=\"checkbox\" id=\""+ (i+1 - blankCountOffset) + "\"> <label for=\""+ (i+1 - blankCountOffset) +"\">" + items[i]  + "</label> </li>");
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
